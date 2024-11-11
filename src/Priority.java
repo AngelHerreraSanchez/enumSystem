817265d968aba5424639a311e828b9c1ec46309d
herreang000 <herreang000@warren.k12.in.us>
2024-11-11 12:17:28 -0500

    final

--- src/Priority.java
+++ src/Priority.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public enum Priority {
+public enum Priority {
+    LOW("Low urgency"),
+    MEDIUM("Medium Urgency"),
+    HIGH("High Urgency");
+
+    private final String description;
+
+
+    Priority(String description) {
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
 }
--- src/Task.java
+++ src/Task.java
@@ -1,2 +1,8 @@
-package PACKAGE_NAME;public class Task {
+public class Task {
+    public static void main(String[] args) {
+        String name = "code";
+        Priority priority = Priority.LOW;
+
+        System.out.println("Ordinal of TASK: " + priority.ordinal());
+    }
 }
